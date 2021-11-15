52ae2f812c7337fc82dc1d39d752364c70296c5e
pl608 <78400949+pl608@users.noreply.github.com>
2021-11-15 09:55:28 -0800

    Create First Try Of New System.py

---
+++ Posts/First Try Of New System.py
@@ -0,0 +1,3 @@
+author = 'Pl608'
+date = '11/15/21'
+content = 'this is a try of the new system for news, dont know if it will ever work sure it will though because you can mess with the html to allow it. I have seen something like this in stackoverflow'
